9911c698df8e12aea1bf26db3007f29c75ad07dc
phnz <phnz@popcorntime.io>
2014-04-25 16:48:20 -0400

    Keep migrating with the local db

--- src/app/lib/providers/eztv.js
+++ src/app/lib/providers/eztv.js
@@ -7,9 +7,8 @@
     var URL = 'http://localhost:5000';
     var URI = require('URIjs');
 
-    var Eztv = function() {
-        this.db = App.db;
-    };
+    var Eztv = function() {};
+
     Eztv.prototype.constructor = Eztv;
 
     var queryTorrents = function(filters) {
@@ -17,18 +16,8 @@
 
         var url = URL;
 
-        if (filters.page) {
-            url = URL + '/shows/' + filters.page;
-        }
+        App.db.getShows(filters, function(err, data) {
 
-        if (filters.keywords) {
-            url = URL + '/shows/search/' + filters.keywords;
-        }
-
-        
-        App.db.getShows({page: filters.page}, function(err, data) {
-            console.log(err);
-            console.log(data);
             deferred.resolve(data || []);
             
         });
@@ -37,68 +26,11 @@
     };
 
     var formatForPopcorn = function(items) {
-
-
-        var movies = {};
-        var movieList = [];
-        _.each(items, function(show) {
-
-            var data = show;
-
-            var largeCover = data.images.poster;
-            var imdb = data.imdb_id.replace('tt', '');
-
-
-            // Calc torrent health
-            var seeds = 0;
-            var peers = 0;
-
-            var ptItem = movies[imdb];
-            if(!ptItem) {
-
-                
-                ptItem = {
-                    imdb:           imdb,
-
-                    title:          data.title.replace(/\([^)]*\)|1080p|DIRECTORS CUT|EXTENDED|UNRATED|3D|[()]/g, ''),
-                    year:           data.year,
-
-                    MovieRating:    data.rating,
-
-                    image:          data.images.poster,
-                    bigImage:       data.images.poster,
-                    backdrop:       resizeImage(data.images.fanart, '940'),
-
-                    runtime:        data.runtime,
-                    synopsis:       data.synopsis,
-
-                    torrents:       data.episodes,
-                    seasonsCount:   0
-                };
-
-
-                movieList.push(ptItem);
-                
-            } else {
-                _.extend(ptItem.episodes, episodes);
-            }
-
-            movies[imdb] = ptItem;
-        });
-
-        return movieList;
-    };
-
-    var resizeImage = function(imageUrl, width) {
-        var uri = URI(imageUrl),
-            ext = uri.suffix(),
-            file = uri.filename().split('.' + ext)[0];
-
-        return uri.filename(file + '-' + width + '.' + ext).toString();
+        return items;
     };
 
     Eztv.prototype.extractIds = function(items) {
-        return _.pluck(items, 'imdb');
+        return _.pluck(items, 'imdb_id');
     };
 
     Eztv.prototype.fetch = function(filters) {
