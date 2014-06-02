81d421591a612daf075ba6e4c06a46cdefd02d47
XeonCore <xeoncore@users.noreply.github.com>
2014-06-02 20:16:09 +1000

    Scale poster according to screen resolution
    Now uses 300px images for the movie/show collection views. If the app detects the user is using a resolution of 720p or greater, it will use the full size poster image in the details view, otherwise it will use the 300px poster. This re-adds the fix for jQuery memory leak. jQuery keeps the reference to the 'orphaned' image tag even after you call remove. (Remove calls DOMElement.remove() which only removes it from the DOM, not from memory)

--- src/app/lib/providers/yts.js
+++ src/app/lib/providers/yts.js
@@ -95,7 +95,6 @@
                     MovieRating: movie.MovieRating,
 
                     image:      largeCover,
-                    bigImage:   largeCover,
 
                     torrents:   torrents
                 };
