7195ab923388be0f016806cc66ae64fa9549b4f3
Sharkiller <sharkale3101@hotmail.com>
2014-06-04 18:04:30 -0300

    Fix hasMore in movies
    Fix #768

--- src/app/lib/providers/yts.js
+++ src/app/lib/providers/yts.js
@@ -1,124 +1,121 @@
 (function(App) {
-    'use strict';
-    var querystring = require('querystring');
-    var request = require('request');
-    var Q = require('q');
+	'use strict';
+	var querystring = require('querystring');
+	var request = require('request');
+	var Q = require('q');
 
-    var URL = false;
-    var Yts = function() {};
+	var URL = false;
+	var Yts = function() {};
 
-    Yts.prototype.constructor = Yts;
+	Yts.prototype.constructor = Yts;
 
-    var queryTorrents = function(filters) {
-        
-        var deferred = Q.defer();
+	var queryTorrents = function(filters) {
 		
-        var params = {};
-        params.sort = 'seeds';
-        params.limit = '50';
-
-        if (filters.keywords) {
-            params.keywords = filters.keywords.replace(/\s/g, '% ');
-        }
-
-        if (filters.genre) {
-            params.genre = filters.genre;
-        }
-
-        if(filters.order) {
-            var order = 'desc';
-            if(filters.order === 1) {
-                order = 'asc';
-            }
-            params.order = order;
-        }
-
-        if (filters.sorter && filters.sorter !== 'popularity') {
-            params.sort = filters.sorter;
-        }
-
-        if (filters.page) {
-            params.set = filters.page;
-        }
-
-        if (Settings.movies_quality !== 'all') {
-            params.quality = Settings.movies_quality;
-        }
+		var deferred = Q.defer();
 		
-        var url = AdvSettings.get('yifyApiEndpoint') + 'list.json?' + querystring.stringify(params).replace(/%E2%80%99/g,'%27');
+		var params = {};
+		params.sort = 'seeds';
+		params.limit = '50';
+
+		if (filters.keywords) {
+			params.keywords = filters.keywords.replace(/\s/g, '% ');
+		}
+
+		if (filters.genre) {
+			params.genre = filters.genre;
+		}
+
+		if(filters.order) {
+			var order = 'desc';
+			if(filters.order === 1) {
+				order = 'asc';
+			}
+			params.order = order;
+		}
+
+		if (filters.sorter && filters.sorter !== 'popularity') {
+			params.sort = filters.sorter;
+		}
+
+		if (filters.page) {
+			params.set = filters.page;
+		}
+
+		if (Settings.movies_quality !== 'all') {
+			params.quality = Settings.movies_quality;
+		}
 		
-        win.info('Request to YTS API');
-        win.debug(url);
-        request({url: url, json: true}, function(error, response, data) {
-            if(error) {
-                deferred.reject(error);
-            } else if(!data || (data.error && data.error !== 'No movies found')) {
-                var err = data? data.error: 'No data returned';
-                console.error('YTS error:', err);
-                deferred.reject(err);
-            } else {
-                deferred.resolve(data.MovieList || []);
-            }
-        });
+		var url = AdvSettings.get('yifyApiEndpoint') + 'list.json?' + querystring.stringify(params).replace(/%E2%80%99/g,'%27');
+		
+		win.info('Request to YTS API');
+		win.debug(url);
+		request({url: url, json: true}, function(error, response, data) {
+			if(error) {
+				deferred.reject(error);
+			} else if(!data || (data.error && data.error !== 'No movies found')) {
+				var err = data? data.error: 'No data returned';
+				win.error('YTS error:', err);
+				deferred.reject(err);
+			} else {
+				deferred.resolve(data.MovieList || []);
+			}
+		});
 
-        return deferred.promise;
-    };
+		return deferred.promise;
+	};
 
-    var formatForPopcorn = function(items) {
-        var movies = {};
-        var movieList = [];
-        _.each(items, function(movie) {
-            if(movie.Quality === '3D') { return; }
-            var largeCover = movie.CoverImage.replace(/_med\./, '_large.');
-            var imdb = movie.ImdbCode.replace('tt', '');
+	var formatForPopcorn = function(items) {
+		var movies = {};
+		var movieFetch = {};
+		movieFetch.movies = [];
+		movieFetch.hasMore = (items.length === 50? true : false);
+		_.each(items, function(movie) {
+			if(movie.Quality === '3D') { return; }
+			var largeCover = movie.CoverImage.replace(/_med\./, '_large.');
+			var imdb = movie.ImdbCode.replace('tt', '');
 
-            // Calc torrent health
-            var seeds = movie.TorrentSeeds;
-            var peers = movie.TorrentPeers;
+			// Calc torrent health
+			var seeds = movie.TorrentSeeds;
+			var peers = movie.TorrentPeers;
 
-            var torrents = {};
-            torrents[movie.Quality] = {
-                url: movie.TorrentUrl,
-                size: movie.SizeByte,
-                seed: seeds,
-                peer: peers
-            };
+			var torrents = {};
+			torrents[movie.Quality] = {
+				url: movie.TorrentUrl,
+				size: movie.SizeByte,
+				seed: seeds,
+				peer: peers
+			};
 
-            var ptItem = movies[imdb];
-            if(!ptItem) {
-                ptItem = {
-                    imdb:       imdb,
+			var ptItem = movies[imdb];
+			if(!ptItem) {
+				ptItem = {
+					imdb:     imdb,
+					title:    movie.MovieTitleClean.replace(/\([^)]*\)|1080p|DIRECTORS CUT|EXTENDED|UNRATED|3D|[()]/g, ''),
+					year:     movie.MovieYear,
+					rating:   movie.MovieRating,
+					image:    largeCover,
+					torrents: torrents
+				};
 
-                    title:      movie.MovieTitleClean.replace(/\([^)]*\)|1080p|DIRECTORS CUT|EXTENDED|UNRATED|3D|[()]/g, ''),
-                    year:       movie.MovieYear,
+				movieFetch.movies.push(ptItem);
+			} else {
+				_.extend(ptItem.torrents, torrents);
+			}
 
-                    rating: movie.MovieRating,
+			movies[imdb] = ptItem;
+		});
+		return movieFetch;
+	};
 
-                    image:      largeCover,
+	Yts.prototype.extractIds = function(items) {
+		return _.pluck(items.movies, 'imdb');
+	};
 
-                    torrents:   torrents
-                };
+	Yts.prototype.fetch = function(filters) {
+		return queryTorrents(filters)
+			.then(formatForPopcorn);
+	};
 
-                movieList.push(ptItem);
-            } else {
-                _.extend(ptItem.torrents, torrents);
-            }
-
-            movies[imdb] = ptItem;
-        });
-
-        return movieList;
-    };
-
-    Yts.prototype.extractIds = function(items) {
-        return _.pluck(items, 'imdb');
-    };
-
-    Yts.prototype.fetch = function(filters) {
-        return queryTorrents(filters)
-            .then(formatForPopcorn);
-    };
-
-    App.Providers.Yts = Yts;
+	App.Providers.Yts = Yts;
 
 })(window.App);
