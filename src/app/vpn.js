7d695d5198d5915ce33ef160681eb8f3da33233f
phnz <phnz@popcorntime.io>
2014-12-17 06:52:03 -0500

    tst

--- src/app/vpn.js
+++ src/app/vpn.js
@@ -33,11 +33,11 @@
 		console.log(process.platform);
 		if (process.platform === 'win32') {
 			var task = require('ms-task');
-			task.pidOf( 'openvpnserv.exe', function(err, data){
+			return task.pidOf( 'openvpnserv.exe', function(err, data){
 				console.log(data);
 				console.log(err);
 				console.log(data.length);
-				if (data.length > 0 && !err) {
+				if (data.length > 0 && err === null) {
 					return true;
 				} else {
 					return false;
