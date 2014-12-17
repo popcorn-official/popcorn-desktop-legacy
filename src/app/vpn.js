d1d0df8f928804033786fbb3faccb1b1e5eb4b69
phnz <phnz@popcorntime.io>
2014-12-17 16:01:18 -0500

    Fix linux command

--- src/app/vpn.js
+++ src/app/vpn.js
@@ -394,7 +394,7 @@
 								if (process.platform === 'linux') {
 
 									var exec = require('child_process').exec;
-									var child = exec(openvpn args.join(" "),
+									var child = exec(openvpn + " " + args.join(" "),
 									  function (error, stdout, stderr) {
 									    console.log('stdout: ' + stdout);
 									    console.log('stderr: ' + stderr);
