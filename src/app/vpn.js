48e44bb15cf3bb3772be2e74f17807bffacfb63a
watdafox <watdafox@popcorntime.io>
2015-02-23 22:17:03 +0100

    jsbeautify && jshint

--- src/app/vpn.js
+++ src/app/vpn.js
@@ -1,11 +1,11 @@
 var request = require('request');
 
-(function(App) {
+(function (App) {
 	'use strict';
 
 	function disconnectIcon() {
 		var vpnButton = $('.vpn-connect');
-		vpnButton.css('color','#266E3E').removeClass('fa-unlock-alt').addClass('fa-lock').attr('data-original-title', i18n.__('Disconnect VPN')).attr('id', 'filterbar-vpn-disconnect');
+		vpnButton.css('color', '#266E3E').removeClass('fa-unlock-alt').addClass('fa-lock').attr('data-original-title', i18n.__('Disconnect VPN')).attr('id', 'filterbar-vpn-disconnect');
 		vpnButton.hover(function () {
 			$(this).addClass('fa-unlock-alt');
 			$(this).removeClass('fa-lock');
@@ -17,7 +17,7 @@ var request = require('request');
 
 	function connectIcon() {
 		var vpnButton = $('.vpn-connect');
-		vpnButton.css('color','#CC0000').removeClass('fa-lock').addClass('fa-unlock-alt').attr('data-original-title', i18n.__('Connect VPN')).attr('id', 'filterbar-vpn-connect');
+		vpnButton.css('color', '#CC0000').removeClass('fa-lock').addClass('fa-unlock-alt').attr('data-original-title', i18n.__('Connect VPN')).attr('id', 'filterbar-vpn-connect');
 		vpnButton.hover(function () {
 			$(this).addClass('fa-lock');
 			$(this).removeClass('fa-unlock-alt');
@@ -27,36 +27,36 @@ var request = require('request');
 		});
 	}
 
-    function VPNClient() {
-    	if (!(this instanceof VPNClient)) {
-    		return new VPNClient();
-    	}
-    	this.connected = false;
-    }
+	function VPNClient() {
+		if (!(this instanceof VPNClient)) {
+			return new VPNClient();
+		}
+		this.connected = false;
+	}
 
 	// open the client
-    VPNClient.prototype.launch = function() {
-    	var vpnClient = gui.Window.open('https://client.vpn.ht/', {
-        	position: 'center',
-        	//frame: false,
-        	focus: true,
-        	toolbar: false,
-        	resizable: false,
-          	width: 500,
-          	height: 500
-        });
-        vpnClient.on('loaded', function(){
-            vpnClient.window.imReady(window);
-        });
-    };
+	VPNClient.prototype.launch = function () {
+		var vpnClient = gui.Window.open('https://client.vpn.ht/', {
+			position: 'center',
+			//frame: false,
+			focus: true,
+			toolbar: false,
+			resizable: false,
+			width: 500,
+			height: 500
+		});
+		vpnClient.on('loaded', function () {
+			vpnClient.window.imReady(window);
+		});
+	};
 	// look if vpn is connected on start
 	// without having to check with the remote client
-	VPNClient.prototype.isRunning = function() {
+	VPNClient.prototype.isRunning = function () {
 		var self = this;
 		// we check if we have at least vpn user and pass
 		// if we have it we can check if we are connected
 		if (App.settings.vpnUsername && App.settings.vpnPassword) {
-			this.getStatus(function(connected) {
+			this.getStatus(function (connected) {
 				self.connected = connected;
 				// update current status
 				if (connected === true) {
@@ -71,7 +71,7 @@ var request = require('request');
 		}
 	};
 	// used to hide in the titlebar
-	VPNClient.prototype.isDisabled = function() {
+	VPNClient.prototype.isDisabled = function () {
 		var disabled = App.settings.vpnDisabledPerm;
 		if (disabled) {
 			return true;
@@ -80,25 +80,25 @@ var request = require('request');
 		}
 	};
 
-	VPNClient.prototype.getStatus = function(callback) {
+	VPNClient.prototype.getStatus = function (callback) {
 		request({
-	  		url: 'https://vpn.ht/status?json'
-	  	}, function(error, response, body) {
+			url: 'https://vpn.ht/status?json'
+		}, function (error, response, body) {
 			if (error) {
 				callback(false);
 			} else if (response.statusCode === 200) {
-		      	body = JSON.parse(body);
-		      	return callback(body.connected);
-		    } else {
-		      	return callback(false);
-		    }
+				body = JSON.parse(body);
+				return callback(body.connected);
+			} else {
+				return callback(false);
+			}
 		});
 	};
 
-	VPNClient.prototype.monitorStatus = function() {
+	VPNClient.prototype.monitorStatus = function () {
 		var self = this;
 		setInterval(function () {
-			self.getStatus(function(status) {
+			self.getStatus(function (status) {
 				self.connected = status;
 				if (status === true) {
 					disconnectIcon();
@@ -111,20 +111,20 @@ var request = require('request');
 	};
 
 	// function exposed to the client as well
-	VPNClient.prototype.setVPNClient = function(Client){
+	VPNClient.prototype.setVPNClient = function (Client) {
 		window.App.VPN = Client;
 	};
 
 	// function called from the VPN client
 	// usefull for user who can't load movie tab
-	VPNClient.prototype.setVPNStatus = function(connected) {
+	VPNClient.prototype.setVPNStatus = function (connected) {
 		this.connected = connected;
 		App.vent.trigger('movies:list');
 	};
 
 	// function called from filter bar view to update icon
 	// with cached value
-	VPNClient.prototype.setVPNStatusCached = function() {
+	VPNClient.prototype.setVPNStatusCached = function () {
 		if (this.connected) {
 			disconnectIcon();
 		} else {
