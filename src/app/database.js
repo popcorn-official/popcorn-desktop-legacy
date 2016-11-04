92107c149c68d87019cb69413d9d1ec6589c8e31
Team Pct <teampct@tuta.io>
2016-11-04 21:39:24 +0100

    Remove Endpoint Checking

--- src/app/database.js
+++ src/app/database.js
@@ -410,9 +410,6 @@ var Database = {
 
                 App.vent.trigger('initHttpApi');
 
-                return AdvSettings.checkApiEndpoints([
-                    Settings.updateEndpoint
-                ]);
             })
             .then(function () {
                 // set app language
